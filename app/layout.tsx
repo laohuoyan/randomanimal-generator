69bb1de5f71570063f616383e4d20f1726890994
Lim <gengliming@bytedance.com>
2024-06-01 12:50:33 +0800

    feat: 配置社交媒体分享信息

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,34 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import SeoScript from "@/components/seo/SeoScript";
+import { BASE_URL } from '@/utils/env';
 
 const inter = Inter({ subsets: ["latin"] });
 
+const description = 'Generated a random animal species: A goat, armadillo, orangutan, porpoise, cheetah... Nearly 200 different animals!';
+
 export const metadata: Metadata = {
   title: "Random Animal Generator",
-  description: "Generated a random animal species: A goat, armadillo, orangutan, porpoise, cheetah... Nearly 200 different animals!",
+  description,
+  alternates: {
+    canonical: '.',
+  },
+  openGraph: {
+    title: "Random Animal Generator",
+    type: 'website',
+    url: BASE_URL,
+    images: `${BASE_URL}/logo.png`,
+    description,
+    siteName: 'Random Animal Generator',
+    locale: 'en_US',
+  },
+  twitter: {
+    site: 'Random Animal Generator',
+    creator: 'CoderLim',
+    description,
+    title: 'Random Animal Generator',
+    images: `${BASE_URL}/logo.png`,
+  }
 };
 
 export default function RootLayout({
